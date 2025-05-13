6fd622f410e93f5391cc37ff96189d99f4c635ca
RKPranav <rkpranav04@gmail.com>
2025-05-13 10:06:12 +0530

    Add files via upload

--- half adder/half_adder_tb.v
+++ half adder/half_adder_tb.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 13.05.2025 09:55:04
+// Create Date: 13.05.2025 10:00:44
 // Design Name: 
-// Module Name: half_adder
+// Module Name: half_adder_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,10 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module half_adder(
-input a,b,
-output s,c
-    );
-    assign s = a^b;
-    assign c = a&b;
+module half_adder_tb;
+reg a,b;
+wire s,c;
+
+half_adder uut(a,b,s,c);
+initial begin
+
+a=0; b=0; #10
+a=0; b=1; #10
+a=1; b=0; #10
+a=1; b=1; #10
+$finish;
+end
 endmodule
