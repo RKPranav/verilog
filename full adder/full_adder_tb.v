48497345fa3438cdff80db0b4c877cf5d1aae70e
RKPranav <rkpranav04@gmail.com>
2025-05-13 09:48:39 +0530

    Add files via upload

--- full adder/full_adder_tb.v
+++ full adder/full_adder_tb.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 13.05.2025 09:22:36
+// Create Date: 13.05.2025 09:32:11
 // Design Name: 
-// Module Name: full_adder
+// Module Name: full_adder_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,10 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module full_adder(
-input a,b,cin,
-output s,cout
-    );
-    assign s = a^b^cin;
-    assign cout = (a & b) | (b & cin) | (a & cin);
+module full_adder_tb;
+reg a,b,cin;
+wire s,cout;
+
+full_adder uut(a,b,cin,s,cout);
+
+initial begin
+
+a=0; b=0; cin=0;#10
+a=0; b=0; cin=1;#10
+a=0; b=1; cin=0;#10
+a=0; b=1; cin=1;#10
+a=1; b=0; cin=0;#10
+a=1; b=0; cin=1;#10
+a=1; b=1; cin=0;#10
+a=1; b=1; cin=1;#10
+$finish;
+end
 endmodule
