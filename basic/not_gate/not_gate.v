90478b4e18be739c0f83369d15503cd798da784e
RKPranav <rkpranav04@gmail.com>
2025-05-21 09:44:06 +0530

    Create not_gate.v

---
+++ basic/not_gate/not_gate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.05.2025 09:35:18
+// Design Name: 
+// Module Name: not_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module not_gate(
+    input a,
+    output y
+    );
+    not(y,a);
+endmodule
