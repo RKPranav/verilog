016c73e09593cf6985df26a45da83d665cfa7f8a
RKPranav <rkpranav04@gmail.com>
2025-05-21 11:07:28 +0530

    Create nor_gate.v

---
+++ basic/nor_gate/nor_gate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.05.2025 11:02:28
+// Design Name: 
+// Module Name: nor_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nor_gate(
+    input a,b,
+    output y
+    );
+    nor(y,a,b);
+endmodule
