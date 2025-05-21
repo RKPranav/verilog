4eda978987043132b66c37ab0ddf7ecc76f7fd71
RKPranav <rkpranav04@gmail.com>
2025-05-21 10:03:31 +0530

    Create or_gate.v

---
+++ basic/or_gate/or_gate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.05.2025 09:56:35
+// Design Name: 
+// Module Name: or_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module or_gate(
+    input a,b,
+    output y
+    );
+    or(y,a,b);
+endmodule
